ee61ab968147864468ea055bef32358f6450034a
AdeelShahid007 <113116957+AdeelShahid007@users.noreply.github.com>
2023-03-14 19:01:49 +0500

    delete

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:uber_scrape/example.dart';
 import 'package:uber_scrape/fare_screen.dart';
 import 'package:uber_scrape/map_screen.dart';
 import 'package:uber_scrape/splash_screen.dart';
-import 'package:uber_scrape/bottom_sheet.dart';
 import 'package:uber_scrape/uber_webview.dart';
 // import 'package:uber_scrape/webView.dart';
 
