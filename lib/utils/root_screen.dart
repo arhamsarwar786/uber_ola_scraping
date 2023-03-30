c3f7ab9810752c776ff29abeae85c2eefc897148
Hassan khan <hssndevsinn@gmail.com>
2023-03-30 16:17:46 +0500

    refacyor

--- lib/utils/root_screen.dart
+++ lib/utils/root_screen.dart
@@ -31,152 +31,197 @@ class _RootScreenState extends State<RootScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: screenList[activeContainerIndex],
-        bottomNavigationBar: Container(
-          color: Colors.white,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              InkWell(
-                onTap: () {
-                  setState(() {
+
+DateTime? currentBackPressTime;
+  Future<bool> _onBackPressed() async {
+    final now = DateTime.now();
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime!) > Duration(seconds: 2)) {
+      currentBackPressTime = now;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Press back again to exit')),
+      );
+      return false;
+    }
+    return true;
+  }
+
+    return WillPopScope(
+        onWillPop: _onBackPressed,
+// onWillPop: ()async{
+
+//   final value =await showDialog<bool>(context: context, builder: (context){
+//     return AlertDialog(
+//       title:const Text("Alert"),
+//       content: const Text("Do you want to exit"),
+//       actions: [
+//         ElevatedButton(onPressed: ()=>
+//         Navigator.of(context).pop(),
+
+//          child: const Text("No"),),
+//           ElevatedButton(onPressed: ()=>
+//         Navigator.of(context).pop(),
+
+//          child: const Text("Exit"),),
+         
+//       ],
+//     );
+//   });
+//   if(value !=null){
+//     return Future.value(value);
+//   }
+//   else{
+//     return Future.value(false);
+//   }
+// },
+
+      child: Scaffold(
+        
+        body: screenList[activeContainerIndex],
+          bottomNavigationBar: Container(
+            color: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      
+                      activeContainerIndex = 0;
+                      
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: selectedScreenIndex[0] == activeContainerIndex ? Colors.purple : Colors.white,
+                          borderRadius: BorderRadius.circular(0),
+                          border: Border.all(
+                            width: 1,
+                            color: activeContainerIndex == 0 ? Colors.purple : Colors.grey,
+                          ),
+                        ),
+    
+                        // color: Colors.purple[900],
+                        child: Icon(
+                          activeContainerIndex == 0 ? Icons.home_sharp : Icons.home_sharp,
+                          size: 35,
+                          color: activeContainerIndex == 0 ? Colors.white : Colors.black,
+                        )
+                        // child:  const Icon(
+                        //   Icons.home_sharp,
+                        //   size: 30.0,
+                        //   color: Colors.white,
+                        // ),
+                        ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
                     
-                    activeContainerIndex = 0;
-                    
-                  });
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(0.0),
+                      activeContainerIndex = 1;
+                      
+                    });
+                  },
                   child: Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        color: selectedScreenIndex[0] == activeContainerIndex ? Colors.purple : Colors.white,
-                        borderRadius: BorderRadius.circular(0),
-                        border: Border.all(
-                          width: 1,
-                          color: activeContainerIndex == 0 ? Colors.purple : Colors.grey,
+                    width: 110,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: selectedScreenIndex[1] == activeContainerIndex ? Colors.purple : Colors.white,
+    
+                      borderRadius: BorderRadius.circular(3),
+                      border: Border.all(
+                        width: 1,
+                        color: activeContainerIndex == 1 ? Colors.purple : Colors.grey,
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(15, 10, 0, 7.5),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage(
+                              'assets/images/ola_icon_full.png',
+                            ),
+                            radius: 15,
+                          ),
                         ),
-                      ),
-
-                      // color: Colors.purple[900],
-                      child: Icon(
-                        activeContainerIndex == 0 ? Icons.home_sharp : Icons.home_sharp,
-                        size: 35,
-                        color: activeContainerIndex == 0 ? Colors.white : Colors.black,
-                      )
-                      // child:  const Icon(
-                      //   Icons.home_sharp,
-                      //   size: 30.0,
-                      //   color: Colors.white,
-                      // ),
-                      ),
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  setState(() {
-                  
-                    activeContainerIndex = 1;
-                    
-                  });
-                },
-                child: Container(
-                  width: 110,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: selectedScreenIndex[1] == activeContainerIndex ? Colors.purple : Colors.white,
-
-                    borderRadius: BorderRadius.circular(3),
-                    border: Border.all(
-                      width: 1,
-                      color: activeContainerIndex == 1 ? Colors.purple : Colors.grey,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Ola",
+                            style: TextStyle(
+                                color: activeContainerIndex == 1
+                                    ? Colors.white
+                                    : Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(15, 10, 0, 7.5),
-                        child: CircleAvatar(
-                          backgroundImage: AssetImage(
-                            'assets/images/ola_icon_full.png',
-                          ),
-                          radius: 15,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "Ola",
-                          style: TextStyle(
-                              color: activeContainerIndex == 1
-                                  ? Colors.white
-                                  : Colors.black,
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
-                  ),
                 ),
-              ),
-              InkWell(
-                onTap: () {
-                  setState(() {
-                   
-                  activeContainerIndex = 2;
-                   
-                  });
-                },
-                child: Container(
-                  width: 110,
-                  height: 60,
-                  decoration: BoxDecoration(
-                                          color: selectedScreenIndex[2] == activeContainerIndex ? Colors.purple : Colors.white,
-
-                    borderRadius: BorderRadius.circular(3),
-                    border: Border.all(
-                      width: 1,
-                      color: activeContainerIndex == 2 ? Colors.purple : Colors.grey,
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                     
+                    activeContainerIndex = 2;
+                     
+                    });
+                  },
+                  child: Container(
+                    width: 110,
+                    height: 60,
+                    decoration: BoxDecoration(
+                                            color: selectedScreenIndex[2] == activeContainerIndex ? Colors.purple : Colors.white,
+    
+                      borderRadius: BorderRadius.circular(3),
+                      border: Border.all(
+                        width: 1,
+                        color: activeContainerIndex == 2 ? Colors.purple : Colors.grey,
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(15, 10, 0, 7.5),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage(
+                              'assets/images/uber_icon_full.png',
+                            ),
+                            radius: 15,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Uber",
+                            style: TextStyle(
+                                color: activeContainerIndex == 2
+                                    ? Colors.white
+                                    : Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(15, 10, 0, 7.5),
-                        child: CircleAvatar(
-                          backgroundImage: AssetImage(
-                            'assets/images/uber_icon_full.png',
-                          ),
-                          radius: 15,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "Uber",
-                          style: TextStyle(
-                              color: activeContainerIndex == 2
-                                  ? Colors.white
-                                  : Colors.black,
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
-                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-     
+       
+      ),
     );
   }
 }
--- lib/willPop.dart
+++ lib/willPop.dart
@@ -43,3 +43,42 @@
 //     );
 //   }
 // }
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: WillPopScope(
+        onWillPop: _onBackPressed,
+        child: Center(
+          child: Text('Press the back button to exit'),
+        ),
+      ),
+    );
+  }
+
+  DateTime? currentBackPressTime;
+  Future<bool> _onBackPressed() async {
+    final now = DateTime.now();
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime!) > Duration(seconds: 2)) {
+      currentBackPressTime = now;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Press back again to exit')),
+      );
+      return false;
+    }
+    return true;
+  }
+}
