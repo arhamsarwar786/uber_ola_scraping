879547fd7dd6f11dd42bca47a14295f168eee554
Hassan khan <hssndevsinn@gmail.com>
2023-04-05 23:48:54 -0700

    refctr

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    
+    debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'TiltNeon', primarySwatch: Colors.purple),
       // home: SearchScreen("demox"),
       // ignore: prefer_const_constructors
