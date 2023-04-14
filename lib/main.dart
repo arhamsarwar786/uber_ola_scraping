250d4628f8a032b3c6ed8788be8d4d66a58e0b37
AdeelShahid007 <113116957+AdeelShahid007@users.noreply.github.com>
2023-04-14 09:57:17 +0500

    updated admods code

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
 // ignore_for_file: unused_import
 
 
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:uber_scrape/cream_webview.dart';
-import 'package:uber_scrape/fare_screen.dart';
-import 'package:uber_scrape/map_screen.dart';
-import 'package:uber_scrape/ola_webview.dart';
-import 'package:uber_scrape/provider/my_provider.dart';
-import 'package:uber_scrape/splash_screen.dart';
-import 'package:uber_scrape/uber_webview.dart';
-import 'package:uber_scrape/utils/color_constants.dart';
-import 'package:uber_scrape/utils/root_screen.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:uber_scrape/helpers/ad_helper.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,23 +13,361 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-   const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-         ChangeNotifierProvider<MyProvider>(create: (context) => MyProvider())
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: 'TiltNeon', primarySwatch: Colors.purple),
-        home:  const SplashScreen(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: const Color(0xff764abc),
+            onPrimary: Colors.white,
+          ),
+        ),
+      ),
+      home: MyHomePage(),
+      //home: const MyHomePage(title: 'AdMob Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static final AdRequest request = AdRequest();
+
+  int maxFailedLoadAttempts = 10;
+  int life = 0;
+  late BannerAd _bannerAd;
+  bool _isBannerAdReady = false;
+
+  // InterstitialAd? _interstitialAd;
+  // int _numInterstitialLoadAttempts = 0;
+
+  RewardedAd? _rewardedAd;
+  int _numRewardedLoadAttempts = 0;
+
+  RewardedInterstitialAd? _rewardedInterstitialAd;
+  int _numRewardedInterstitialLoadAttempts = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadBannerAd();
+    // _createInterstitialAd();
+    _createRewardedAd();
+    _createRewardedInterstitialAd();
+  }
+
+  // Ads creation starts here
+  void _loadBannerAd() {
+    _bannerAd = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdReady = true;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          _isBannerAdReady = false;
+          ad.dispose();
+        },
+      ),
+    );
+
+    _bannerAd.load();
+  }
+
+  // void _createInterstitialAd() {
+  //   InterstitialAd.load(
+  //       adUnitId: AdHelper.interstitialAdUnitId,
+  //       request: request,
+  //       adLoadCallback: InterstitialAdLoadCallback(
+  //         onAdLoaded: (InterstitialAd ad) {
+  //           print('$ad loaded');
+  //           _interstitialAd = ad;
+  //           _numInterstitialLoadAttempts = 0;
+  //           _interstitialAd!.setImmersiveMode(true);
+  //         },
+  //         onAdFailedToLoad: (LoadAdError error) {
+  //           print('InterstitialAd failed to load: $error.');
+  //           _numInterstitialLoadAttempts += 1;
+  //           _interstitialAd = null;
+  //           if (_numInterstitialLoadAttempts < maxFailedLoadAttempts) {
+  //             _createInterstitialAd();
+  //           }
+  //         },
+  //       ));
+  // }
+
+  void _createRewardedAd() {
+    RewardedAd.load(
+        adUnitId: AdHelper.rewardedAdUnitId,
+        request: request,
+        rewardedAdLoadCallback: RewardedAdLoadCallback(
+          onAdLoaded: (RewardedAd ad) {
+            print('$ad loaded.');
+            _rewardedAd = ad;
+            _numRewardedLoadAttempts = 0;
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            print('RewardedAd failed to load: $error');
+            _rewardedAd = null;
+            _numRewardedLoadAttempts += 1;
+            if (_numRewardedLoadAttempts < maxFailedLoadAttempts) {
+              _createRewardedAd();
+            }
+          },
+        ));
+  }
+
+  void _createRewardedInterstitialAd() {
+    RewardedInterstitialAd.load(
+        adUnitId: AdHelper.rewardedInterstitialAd,
+        request: request,
+        rewardedInterstitialAdLoadCallback: RewardedInterstitialAdLoadCallback(
+          onAdLoaded: (RewardedInterstitialAd ad) {
+            print('$ad loaded.');
+            _rewardedInterstitialAd = ad;
+            _numRewardedInterstitialLoadAttempts = 0;
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            print('RewardedInterstitialAd failed to load: $error');
+            _rewardedInterstitialAd = null;
+            _numRewardedInterstitialLoadAttempts += 1;
+            if (_numRewardedInterstitialLoadAttempts < maxFailedLoadAttempts) {
+              _createRewardedInterstitialAd();
+            }
+          },
+        ));
+  }
+
+  // Showing ads from here
+  // void _showInterstitialAd() {
+  //   if (_interstitialAd == null) {
+  //     print('Warning: attempt to show interstitial before loaded.');
+  //     return;
+  //   }
+  //   _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+  //     onAdShowedFullScreenContent: (InterstitialAd ad) =>
+  //         print('ad onAdShowedFullScreenContent.'),
+  //     onAdDismissedFullScreenContent: (InterstitialAd ad) {
+  //       print('$ad onAdDismissedFullScreenContent.');
+  //       ad.dispose();
+  //       _createInterstitialAd();
+  //     },
+  //     onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+  //       print('$ad onAdFailedToShowFullScreenContent: $error');
+  //       ad.dispose();
+  //       _createInterstitialAd();
+  //     },
+  //   );
+  //   _interstitialAd!.show();
+  //   _interstitialAd = null;
+  // }
+
+void _showRewardedAd() {
+  if (_rewardedAd == null) {
+    // If the rewarded ad is null, load it and return.
+    _createRewardedAd();
+    return;
+  }
+
+  _rewardedAd!.fullScreenContentCallback = FullScreenContentCallback(
+    onAdShowedFullScreenContent: (RewardedAd ad) =>
+        print('ad onAdShowedFullScreenContent.'),
+    onAdDismissedFullScreenContent: (RewardedAd ad) {
+      print('$ad onAdDismissedFullScreenContent.');
+      ad.dispose();
+      _createRewardedAd();
+    },
+    onAdFailedToShowFullScreenContent: (RewardedAd ad, AdError error) {
+      print('$ad onAdFailedToShowFullScreenContent: $error');
+      ad.dispose();
+      _createRewardedAd();
+    },
+  );
+
+  _rewardedAd!.setImmersiveMode(true);
+  _rewardedAd!.show(
+      onUserEarnedReward: (AdWithoutView ad, RewardItem reward) {
+    setState(() {
+      life += 1;
+    });
+  });
+  _rewardedAd = null;
+}
+
+  void _showRewardedInterstitialAd() {
+    if (_rewardedInterstitialAd == null) {
+      print('Warning: attempt to show rewarded interstitial before loaded.');
+      return;
+    }
+    _rewardedInterstitialAd!.fullScreenContentCallback =
+        FullScreenContentCallback(
+      onAdShowedFullScreenContent: (RewardedInterstitialAd ad) =>
+          print('$ad onAdShowedFullScreenContent.'),
+      onAdDismissedFullScreenContent: (RewardedInterstitialAd ad) {
+        print('$ad onAdDismissedFullScreenContent.');
+        ad.dispose();
+        _createRewardedInterstitialAd();
+      },
+      onAdFailedToShowFullScreenContent:
+          (RewardedInterstitialAd ad, AdError error) {
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        ad.dispose();
+        _createRewardedInterstitialAd();
+      },
+    );
+
+    _rewardedInterstitialAd!.setImmersiveMode(true);
+    _rewardedInterstitialAd!.show(
+        onUserEarnedReward: (AdWithoutView ad, RewardItem reward) {
+      print('$ad with reward $RewardItem(${reward.amount}, ${reward.type})');
+    });
+    _rewardedInterstitialAd = null;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bannerAd.dispose();
+    // _interstitialAd?.dispose();
+    _rewardedAd?.dispose();
+    _rewardedInterstitialAd?.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AdMob Demo'),
+        backgroundColor: const Color(0xff764abc),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Life',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      size: 30,
+                      color: Colors.redAccent,
+                    ),
+                    Text(
+                      '$life',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // _showInterstitialAd();
+                  },
+                  child: Text(
+                    ' InterstitialAd',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _showRewardedAd();
+                  },
+                  child: Text(
+                    ' RewardedAd',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                // ElevatedButton(
+                //   onPressed: () {
+                //     _showRewardedInterstitialAd();
+                //   },
+                //   child: Text(
+                //     'RewardedInterstitialAd',
+                //     style: TextStyle(fontSize: 30),
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+          if (_isBannerAdReady)
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: _bannerAd.size.width.toDouble(),
+                height: _bannerAd.size.height.toDouble(),
+                child: AdWidget(ad: _bannerAd),
+              ),
+            ),
+        ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:uber_scrape/cream_webview.dart';
+// import 'package:uber_scrape/fare_screen.dart';
+// import 'package:uber_scrape/map_screen.dart';
+// import 'package:uber_scrape/ola_webview.dart';
+// import 'package:uber_scrape/provider/my_provider.dart';
+// import 'package:uber_scrape/splash_screen.dart';
+// import 'package:uber_scrape/uber_webview.dart';
+// import 'package:uber_scrape/utils/color_constants.dart';
+// import 'package:uber_scrape/utils/root_screen.dart';
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   MobileAds.instance.initialize();
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//    const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiProvider(
+//       providers: [
+//          ChangeNotifierProvider<MyProvider>(create: (context) => MyProvider())
+//       ],
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         theme: ThemeData(fontFamily: 'TiltNeon', primarySwatch: Colors.purple),
+//         home:  const SplashScreen(),
+//       ),
+//     );
+//   }
+// }
 
 
 
